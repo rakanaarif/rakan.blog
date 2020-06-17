5ac6f4d88698a8bfc244f6694a4032cc74f56db7
rakanaarif <42450257+rakanaarif@users.noreply.github.com>
2020-06-18 01:36:03 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,11 +1,4 @@
-
-    (function() {
-      var items = <data:post.commentJso/>;
-      var msgs = <data:post.commentMsgs/>;
-      var config = <data:post.commentConfig/>;
-
-// <![CDATA[
-      var cursor = null;
+ var cursor = null;
       if (items && items.length > 0) {
         cursor = parseInt(items[items.length - 1].timestamp) + 1;
       }
@@ -173,5 +166,3 @@
         window.goog.comments.loadQueue.push(render);
       }
     })();
-// ]]>
-  
